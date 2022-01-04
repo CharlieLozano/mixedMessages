2422cfcd6cecccc86f5aed204a6ecadf13d8e091
Chears <caolort@hotmail.com>
2022-01-04 11:56:48 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Welcome to Silly Tabletop RPG")
+
+const races = [
+    'Candyman',
+    'Sugar Daddy',
+    'Burger Flipper Wizard',
+    'Pokemon Card Collector',
+    'Left Socks Thief'
+]
+
+const places = [
+    'KEE KUU CUACK CUACK',
+    'Apple\'s Kindergarten',
+    'Walmart',
+    'an unfinished tree house',
+    'Landyland',
+    '... I forgot where'
+]
+
+const mentors = [
+    'Obama',
+    'Shrek',
+    'your Mom',
+    'one cartoon singing frog',
+    'Chears',
+    'Jimi Hendrix'
+]
+
+let race = races[Math.floor(Math.random() * races.length)]
+let place = places[Math.floor(Math.random() * places.length)]
+let mentor = mentors[Math.floor(Math.random() * mentors.length)]
+
+console.log(`You are a ${race} trained by ${mentor} in the lands of ${place}. `)
